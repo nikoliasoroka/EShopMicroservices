af79bf815b60fb80d000874552b4e47cc5a83dc6
Nikolia Soroka <nikoliasoroka@gmail.com>
2025-01-16 19:44:47 +0200

    Basket.API implementation

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddCarter();
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
