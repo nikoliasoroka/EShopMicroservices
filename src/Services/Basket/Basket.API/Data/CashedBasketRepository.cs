1e4cac4158884c9ce01c136556809bffc142e335
Nikolia Soroka <nikoliasoroka@gmail.com>
2025-01-17 13:14:40 +0200

    Added Redis cashe and health-checks

---
+++ src/Services/Basket/Basket.API/Data/CashedBasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data;
+
+public class CashedBasketRepository(IBasketRepository repository, IDistributedCache cashe)
+    : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var cashedBasket = await cashe.GetStringAsync(userName, cancellationToken);
+
+        if (!string.IsNullOrEmpty(cashedBasket))
+            return JsonSerializer.Deserialize<ShoppingCart>(cashedBasket)!;
+
+        var basket = await repository.GetBasket(userName, cancellationToken);
+        await cashe.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        await repository.StoreBasket(basket, cancellationToken);
+        await cashe.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        await repository.DeleteBasket(userName, cancellationToken);        
+        await cashe.RemoveAsync(userName, cancellationToken);
+
+        return true;
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 var assembly = typeof(Program).Assembly;
-var databaseConnection = builder.Configuration.GetConnectionString("Database");
+
+var connections = new
+{
+    Database = builder.Configuration.GetConnectionString("Database")!,
+    Redis = builder.Configuration.GetConnectionString("Redis")!
+};
 
 builder.Services.AddMediatR(config =>
 {
@@ -16,13 +20,23 @@ builder.Services.AddCarter();
 
 builder.Services.AddMarten(options =>
 {
-    options.Connection(databaseConnection!);
+    options.Connection(connections.Database);
     options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CashedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = connections.Redis;
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connections.Database)
+    .AddRedis(connections.Redis);
 
 // Configure the HTTP request pipeline.
 var app = builder.Build();
@@ -30,5 +44,11 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(option => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
