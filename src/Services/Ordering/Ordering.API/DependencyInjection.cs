d5d29ec734f3077c83c6ae8050e5cc3100f5c6a0
Nikolia Soroka <nikoliasoroka@gmail.com>
2025-02-15 16:46:53 +0200

    Implemented Ordering.API

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,17 +1,19 @@
-﻿namespace Ordering.API;
+﻿using Carter;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AppApiServices(this IServiceCollection services)
     {
-        //serives.AddCarter();
+        services.AddCarter();
 
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
-        //app.MapCarter();
+        app.MapCarter();
 
         return app;
     }
