5cb6d7a065af34c80d0063e7d384caffebeb7290
Nikolia Soroka <nikoliasoroka@gmail.com>
2025-03-13 11:46:06 +0200

    Added Docker configuration for Ordering API

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -10,7 +10,7 @@ public record OrderName
     public static OrderName Of(string value)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(value);
-        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+        //ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
 
         return new OrderName(value);
     }
