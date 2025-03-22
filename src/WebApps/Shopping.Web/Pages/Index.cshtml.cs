60789881883622553d5521ca705e73950efc0023
Nikolia Soroka <nikoliasoroka@gmail.com>
2025-03-22 20:12:25 +0200

    Added Index page

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,18 +1,18 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace Shopping.Web.Pages;
-public class IndexModel : PageModel
+
+public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logger) 
+    : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public async Task<IActionResult> OnGetAsync()
     {
-        _logger = logger;
-    }
+        logger.LogInformation("Index page visited");
 
-    public void OnGet()
-    {
+        var result = await catalogService.GetProducts();
 
+        ProductList = result.Products;
+
+        return Page();
     }
 }
